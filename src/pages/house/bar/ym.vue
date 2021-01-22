ce03b5c1531d344f691621a14ba66873bdaab61f
lyb615 <316439904@qq.com>
2021-01-22 11:42:49 +0800

    房屋修改

---
+++ src/pages/house/bar/ym.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="u-lazy-load">
+		<u-lazy-load v-for="(item, index) in array" :key="index" :image="item.src"></u-lazy-load>
+		<u-lazy-load>
+			<image :src="item.src" mode="scaleToFill" style="width: 65rpx;height: 65rpx;"></image>
+		</u-lazy-load>
+		<view class="price">
+			<text>￥{{item.price}}</text>
+			<text></text>
+		</view>
+		<view class="decriction">
+			<text>{{item.dec}}</text>
+		</view>
+		
+		
+	
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				// 设计成数组内嵌对象而不是纯数组形式，是考虑到真实环境后端返回的数据为如此形式
+				array: [{
+						src: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.myfun7.com%2Foss%2Fonline%2FsearchPhotoLeaseZj%2F2018%2F06%2F17%2Fc0d51d5c0b1448afad14ed71038cfe71.jpg%3Fx-oss-process%3Dimage%2Fresize%2Cm_fill%2Ch_421%2Cw_712%2Climit_0&refer=http%3A%2F%2Fpic.myfun7.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613803683&t=b2c65a31f3cc1e9a64083239d2195d4b",
+						},
+					{
+						src: "https://pic.uj.cn/oss/online/tmp/2020/08/27/0edf431941414534808e4abde55117ea.jpg?x-oss-process=image/resize,m_fill,h_421,w_712,limit_0/watermark,image_d20vdWprMS5wbmc=,t_8,g_center",
+					
+					},
+					{
+						src: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fn.sinaimg.cn%2Fsinacn20118%2F273%2Fw640h433%2F20181219%2F888e-hqnkypr2092170.jpg&refer=http%3A%2F%2Fn.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613802866&t=ecb193c79aec484af7caea6c54f070fc",
+						
+					},
+					{
+						
+						src: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.58cdn.com.cn%2Fmobile%2Fbig%2Fn_v2f98be9eea9754303bcb234413e0a5259.jpg&refer=http%3A%2F%2Fpic2.58cdn.com.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613803875&t=ad0c2ddd7896ec065c9cd2ad57aefb3a",
+						
+					},
+				]
+			}
+		}
+	}
+</script>
+<style lang="scss">
+.u-lazy-load {
+  background-color: #eee;
+  overflow: hidden;
+   width: 30%;
+  -webkit-transform: transition3d(0, 0, 0);
+          transform: transition3d(0, 0, 0);
+  will-change: transform;
+ margin-left: 26rpx;
+  padding: 35rpx;
+
+
+}	
+	
+	
+</style>
---
+++ src/pages/house/bar/zuf.vue
@@ -0,0 +1,172 @@
+<template>
+	<view>
+		<view class="search">
+			<u-search placeholder="请输入房屋信息"></u-search>
+		</view>
+	</view>
+	<view class="aa">
+		<view class="u-demo">
+			<view class="u-demo-wrap">
+				<view class="u-demo-area">
+					<u-grid :col="col" @click="click" v-if="!isSwiper" :border="border">
+						<u-grid-item name="item1" :index="0" @click="itemClick">
+							<u-badge count="9" :offset="[col == 3 ? 20 : 14, col == 3 ? 30 : 20]"></u-badge>
+							<u-icon name="photo" :size="46"></u-icon>
+							<view class="grid-text">二手房</view>
+						</u-grid-item>
+						<u-grid-item :index="1">
+							<u-icon name="lock" :size="46"></u-icon>
+							<view class="grid-text">新房</view>
+						</u-grid-item>
+						<u-grid-item :index="2">
+							<image src="https://cdn.uviewui.com/uview/grid/hot5.png" :style="{
+								top: col == 3 ? '14rpx' : '8rpx',
+								right: col == 3 ? '40rpx' : '28rpx'
+							}" style="width: 34rpx;height: 34rpx;" class="badge-icon" mode="widthFix"></image>
+							<u-icon name="hourglass" :size="46"></u-icon>
+							<view class="grid-text">租房</view>
+						</u-grid-item>
+						<u-grid-item :index="3">
+							<u-icon name="home" :size="46"></u-icon>
+							<view class="grid-text">出售</view>
+						</u-grid-item>
+						<u-grid-item :index="4">
+							<u-icon name="star" :size="46"></u-icon>
+							<view class="grid-text">找小区</view>
+						</u-grid-item>
+						<u-grid-item :index="5">
+							<u-icon name="volume-up" :size="46"></u-icon>
+							<view class="grid-text">写字楼</view>
+						</u-grid-item>
+						<u-grid-item :index="6">
+							<image src="https://cdn.uviewui.com/uview/grid/hot6.png" style="width: 44rpx;height: 44rpx;top:0;right:0;" class="badge-icon"></image>
+							<u-icon name="trash" :size="46"></u-icon>
+							<view class="grid-text">厂房仓库</view>
+						</u-grid-item>
+						<u-grid-item :index="7">
+							<u-icon name="rewind-right" :size="46"></u-icon>
+							<view class="grid-text">我是业主</view>
+						</u-grid-item>
+						<u-grid-item :index="8">
+							<u-icon name="shopping-cart" :size="46"></u-icon>
+							<view class="grid-text">找经纪人</view>
+						</u-grid-item>
+					</u-grid>
+					<swiper class="swiper" v-else @change="change">
+						<swiper-item>
+							<u-grid :border="border" :col="col" @click="click" hover-class="hover-class">
+								<u-grid-item v-for="(item, index) in list" :index="index" :key="index">
+									<u-icon :name="item" :size="46"></u-icon>
+									<text class="grid-text">{{ '宫格' + (index + 1) }}</text>
+								</u-grid-item>
+							</u-grid>
+						</swiper-item>
+						<swiper-item>
+							<u-grid :border="border" :col="col" @click="click">
+								<u-grid-item v-for="(item, index) in list" :index="index + 9" :key="index">
+									<u-icon :name="item" :size="46"></u-icon>
+									<text class="grid-text">{{ '宫格' + (index + 1) }}</text>
+								</u-grid-item>
+							</u-grid>
+						</swiper-item>
+						<swiper-item>
+							<u-grid :border="border" :col="col" @click="click">
+								<u-grid-item v-for="(item, index) in list" :index="index + 18" :key="index">
+									<u-icon :name="item" :size="46"></u-icon>
+									<text class="grid-text">{{ '宫格' + (index + 1) }}</text>
+								</u-grid-item>
+							</u-grid>
+						</swiper-item>
+					</swiper>
+					<view class="indicator-dots" v-if="isSwiper">
+						<view class="indicator-dots-item" :class="[current == 0 ? 'indicator-dots-active' : '']">
+						</view>
+						<view class="indicator-dots-item" :class="[current == 1 ? 'indicator-dots-active' : '']">
+						</view>
+						<view class="indicator-dots-item" :class="[current == 2 ? 'indicator-dots-active' : '']">
+						</view>
+					</view>
+				</view>
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['integral', 'kefu-ermai', 'coupon', 'gift', 'scan', 'pause-circle', 'wifi', 'email', 'list'],
+				isSwiper: false,
+				current: 0,
+				border: true,
+				col: 3
+			};
+		},
+		methods: {
+			isSwiperChange(index) {
+				this.isSwiper = index == 0 ? true : false;
+			},
+			borderChange(index) {
+				this.border = index == 0 ? true : false;
+			},
+			colChange(index) {
+				this.col = index == 0 ? 3 : 4;
+			},
+			click(index) {
+				this.$refs.uToast.show({
+					title: `点击了第${index + 1}宫格`,
+					type: 'warning'
+				})
+			},
+			change(e) {
+				this.current = e.detail.current;
+			},
+			// 针对单个grid-item的事件
+			itemClick(index) {
+				console.log("============");
+			}
+		}
+	};
+</script>
+
+<style scoped lang="scss">
+	.aa{
+		margin-top: 60rpx;
+	}
+.grid-text {
+	font-size: 28rpx;
+	margin-top: 4rpx;
+	color: $u-type-info;
+}
+
+.badge-icon {
+	position: absolute;
+	width: 40rpx;
+	height: 40rpx;
+}
+
+.swiper {
+	height: 480rpx;
+}
+
+.indicator-dots {
+	margin-top: 40rpx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.indicator-dots-item {
+	background-color: $u-tips-color;
+	height: 6px;
+	width: 6px;
+	border-radius: 10px;
+	margin: 0 3px;
+}
+
+.indicator-dots-active {
+	background-color: $u-type-primary;
+}
+</style>
