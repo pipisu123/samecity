61885930ff61a9029365034115b0b1a086711022
lyb615 <316439904@qq.com>
2021-01-22 11:30:55 +0800

    房屋修改

--- src/pages/house/house.vue
+++ src/pages/house/house.vue
@@ -1,66 +1,63 @@
 <template>
-	<view >
-		<text>公司福利</text>
-		<ld-select :multiple="true" :list="options"
-		list-key="label" value-key="value"
-		placeholder="请选择"
-		clearable
-		v-model="value2"
-		@change="selectChange2"></ld-select>
-		<swipper></swipper>
+	<view>
+		
+		<view class="search">
+			<u-search placeholder="请输入房屋信息"></u-search>
+		</view>
+	<Grid></Grid>
+	<swipper></swipper>
+	<fz></fz>
+	<ym></ym>
+	<bar></bar>
+		
 	</view>
-	
 </template>
 
 <script>
-	import ldSelect from './ld-select.vue'
+	import bar from './bar/bar.vue';
+	import Grid from './bar/Grid.vue'
 	import swipper from '../components/swipper.vue'
+	import fz from './bar/fz.vue'
+	import ym from './bar/ym.vue'
+	
+	
 	export default {
 		data() {
 			return {
-				 value: '',
-				                options: [{
-				                  value: '选项1',
-				                  label: '黄金糕'
-				                }, {
-				                  value: '选项2',
-				                  label: '双皮奶'
-				                }, {
-				                  value: '选项3',
-				                  label: '蚵仔煎'
-				                }, {
-				                  value: '选项4',
-				                  label: '龙须面'
-				                }, {
-				                  value: '选项5',
-				                  label: '北京烤鸭'
-				                }],
-				                value2: ['选项2','选项4'],
-				                value3: ['选项1','选项2'],
-				                value4: []
+				
 			}
 		},
 		 components: {
-		            ldSelect,
-					swipper
+		          bar,
+				  Grid,
+				  swipper,
+				  fz,
+				  ym
+				 
+				
+				 
 		        },
 		methods: {
-			 selectChange(val){
-			                this.value = val
-			            },
-			            selectChange2(val){
-			                this.value2 = val
-			            },
-			            selectChange4(val){
-			                this.value4 = val
-			            },
-			            selectChange5(val){
-			                this.value5 = val
-			            }
+			hreftwo(){
+				this.$router.push({
+					path:'/bar/fb.vue'
+				})
+			}
+			
+			
 		}
 	}
 </script>
 
-<style>
-	
+<style lang="scss">
+	.search{
+		border-color: #fff;
+	    position: fixed;
+	     left: 0;
+	     right: 0;
+	     top: 0;
+	     z-index: 9;
+		 background-color: #5785E5;
+	     padding-bottom: 10rpx;
+	}		
 </style>
